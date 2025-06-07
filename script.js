dba539678bd476670f94a4c5bf0193868182c150
Levizera <levigames321@gmail.com>
2025-06-07 05:12:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Inicializa a biblioteca AOS (Animate On Scroll)
+AOS.init({
+    duration: 1000, // Duração da animação em milissegundos
+    once: true,     // A animação acontece apenas uma vez
+    offset: 50,     // Distância do elemento para o início da animação
+});
